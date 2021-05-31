29aa680269218976368ea6a45870ee54f169fcc0
Isaac23-K <ikaptoge@yahoo.com>
2021-05-31 18:03:08 +0300

    changed code in encoding

--- src/main/java/Encode.java
+++ src/main/java/Encode.java
@@ -1,2 +1,26 @@
 public class Encode {
+
+    public static StringBuffer encoding(String plainText, int key){
+
+        //Encode the text
+        StringBuffer encode = new StringBuffer();
+        for (int i = 0; i < plainText.length(); i++){
+
+            //Check if text is upper case
+            if (Character.isUpperCase(plainText.charAt(i))){
+                char c = (char) (((int)plainText.charAt(i) + key -65) % 26 + 65);
+                encode.append(c);
+
+                //Check if text is lower case
+            } else {
+                char c = (char) (((int)plainText.charAt(i) + key -97) % 26 + 97);
+                encode.append(c);
+
+            }
+        }
+
+        return encode;
+
+    }
+
 }
