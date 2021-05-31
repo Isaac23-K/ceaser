debe53dfb6d3b881c781b18599246576d36f6172
Isaac23-K <ikaptoge@yahoo.com>
2021-05-31 18:03:38 +0300

    made changes to the decode class

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,1 +1,25 @@
+public class Decode {
 
+    public static StringBuffer decoding(String cipherText, int shift){
+
+        //Decrypt the text
+        StringBuffer decode = new StringBuffer();
+        for (int j = 0; j < cipherText.length(); j++){
+
+            //Check if text is upper case
+            if (Character.isUpperCase(cipherText.charAt(j))){
+                char d = (char) (((int)cipherText.charAt(j) - shift -65) % 26 + 65);
+                decode.append(d);
+
+                //Check if text is lower case
+            } else {
+                char d = (char) (((int)cipherText.charAt(j) - shift -97) % 26 + 97);
+                decode.append(d);
+
+            }
+        }
+
+        return decode;
+
+    }
+}
